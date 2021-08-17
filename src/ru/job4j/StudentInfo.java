19cdb03955649d708e2185d0692a933068fd736c
Komarov92 <Komarov2192@mail.ru>
2021-08-17 22:57:07 +0700

    3.3. Разбор первой программы. [#159033]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,4 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anton Komarov");
+        System.out.println("21.11.1992");
+    }
 }
