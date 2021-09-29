668ff774526c0ba8c9d35cbd1652ccf7b94d28a9
Komarov92 <Komarov2192@mail.ru>
2021-09-29 22:27:42 +0700

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -6,8 +6,8 @@ public class DummyBot {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-            } else if ("Как дела?".equals(question)) {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+            } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
         }
         return rsl;
     }
