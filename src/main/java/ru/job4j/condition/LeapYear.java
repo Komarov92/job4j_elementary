0d4bdf8c6559a51eec312e396b056810933abfba
Komarov92 <Komarov2192@mail.ru>
2021-10-27 00:06:55 +0700

    Високосный год

--- src/main/java/ru/job4j/condition/LeapYear.java
+++ src/main/java/ru/job4j/condition/LeapYear.java
@@ -2,14 +2,6 @@ package ru.job4j.condition;
 
 public class LeapYear {
     public static boolean checkYear(int year) {
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0));
     }
 }
