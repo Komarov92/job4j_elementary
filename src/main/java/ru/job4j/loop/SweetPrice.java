dde4483baf3e00961ad002ad21f57ba06ee1a311
Komarov92 <Komarov2192@mail.ru>
2021-11-01 01:40:02 +0700

    Конфеты

---
+++ src/main/java/ru/job4j/loop/SweetPrice.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class SweetPrice {
+    public static void out(int price) {
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum = price * i;
+            System.out.println(sum);
+        }
+    }
+}
+
