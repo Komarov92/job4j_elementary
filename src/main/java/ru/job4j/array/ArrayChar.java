3f959151b193b3eba2bc8e073b09df2d8bd7106d
Komarov92 <Komarov2192@mail.ru>
2021-12-07 00:57:56 +0700

    6.4. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 1; i < pref.length; i++) {
+            if (word[i] != pref[1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
